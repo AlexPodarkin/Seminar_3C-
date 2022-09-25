574ae2b95e82b71a0cbc13ec905fa8a4166fe865
Aleksandr <alexchembarov@gmail.com>
2022-09-25 12:37:40 +0400

    HomeworkWithAStar

---
+++ HomeworkWithAStar/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine($"Программа принимает радиус круга и находит его площадь округленную до целого числа");
+Console.Write($"введите радиус круга:");
+double radius = Convert.ToDouble(Console.ReadLine());
+double areaCircle = Math.PI * Math.Pow(radius, 2);
+Console.WriteLine(areaCircle);
+areaCircle = areaCircle % 10;
+Console.WriteLine(areaCircle);
+double result = Math.Round(areaCircle);
+Console.WriteLine(result);
+Console.Write($"округленная площадь  = {result}");
+
